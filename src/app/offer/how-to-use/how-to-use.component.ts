76c6cba32129a0f6b9471d3fa7e867b1cf2b0e27
Daniel Camucatto <dcamucatto@dw3.office>
2022-12-26 18:53:32 -0300

    add rotas em howToUSe

--- src/app/offer/how-to-use/how-to-use.component.ts
+++ src/app/offer/how-to-use/how-to-use.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OffersService } from 'src/app/offers.service';
+
 
 @Component({
   selector: 'app-how-to-use',
   templateUrl: './how-to-use.component.html',
-  styleUrls: ['./how-to-use.component.css']
+  styleUrls: ['./how-to-use.component.css'],
+  providers: [OffersService]
 })
 export class HowToUseComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public howToUse: string = '';
+
+  constructor(private route: ActivatedRoute, private offersService: OffersService) { }
 
   ngOnInit(): void {
-    console.log('ID rota pai:' , this.route.parent?.snapshot.params['id'] );
+    this.offersService.getHowToUseforId(this.route.parent?.snapshot.params['id'])
+    .then((resp:string) => {this.howToUse = resp} )
     
   }
 
--- src/app/offers.service.ts
+++ src/app/offers.service.ts
@@ -20,13 +20,21 @@ export class OffersService {
     }
 
     public getOffersForCategory(category: string): Promise<Offers[]>{
-        return firstValueFrom(this.http.get(`${URL_API}?categoria=${category}`))
+        return firstValueFrom(this.http.get(`${URL_API}/ofertas?categoria=${category}`))
             .then((resp: any) => resp)
     }
     
     public getOffersForId(id: Number):Promise<Offers> {
-        return firstValueFrom(this.http.get(`${URL_API}?id=${id}`))
+        return firstValueFrom(this.http.get(`${URL_API}/ofertas?id=${id}`))
         .then((resp: any) => resp[0]) 
+        
+    }
+
+    public getHowToUseforId(id: number): Promise<string>{
+        return firstValueFrom(this.http.get(`${URL_API}/como-usar?id=${id}`))
+        .then((resp: any) => {
+            return resp[0].descricao;
+        })
         
     }
 
