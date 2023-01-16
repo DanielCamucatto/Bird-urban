7a3a539c36961fe184dc1d8380408c2ba8dc6a04
Daniel Camucatto <dcamucatto@dw3.office>
2023-01-16 16:37:58 -0300

    observable na prática

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Offers } from '../shared/offers.model';
 import { OffersService } from '../offers.service';
+import { interval, Observer, Observable } from 'rxjs';
+
 
 
 @Component({
@@ -30,5 +32,16 @@ export class OfferComponent implements OnInit {
     // (erro: any) => console.log(erro), 
     // () => console.log('processamento concluído!!')
     // )
+
+    let myObservableTest = new Observable((observer: Observer<number>) =>{
+      observer.next(1)
+      observer.next(3)
+    });
+
+    myObservableTest.subscribe((result: number) => {
+      console.log(result + 30);
+      
+    })
+
   }
 }
